ff2c1a30f25227af03ababc3a9924b07b278bf14
Bricktheworld <brandon.shihabi@gmail.com>
2021-06-11 16:02:07 -0700

    Updated game engine

--- res/shaders/forward_material.glsl
+++ res/shaders/forward_material.glsl
@@ -11,12 +11,11 @@ out vec3 FragPos;
 out vec2 FragUV;
 
 uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
+uniform mat4 MVP;
 
 void main() 
 {
-    gl_Position = projection * view * model * vec4(position, 1.0f);
+    gl_Position = MVP * vec4(position, 1.0f);
     FragPos = vec3(model * vec4(position, 1.0f));
     Normal  = mat3(transpose(inverse(model))) * normal;
     FragUV = vertexUV;
--- res/shaders/material.glsl
+++ res/shaders/material.glsl
@@ -10,14 +10,12 @@ out vec3 Normal;
 out vec3 FragPos;
 out vec2 FragUV;
 
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
+uniform mat4 MVP;
 uniform int texture;
 
 void main() 
 {
-    gl_Position = projection * view * model * vec4(position, 1.0f);
+    gl_Position = MVP * vec4(position, 1.0f);
     FragPos = vec3(model * vec4(position, 1.0f));
     Normal  = mat3(transpose(inverse(model))) * normal;
     FragUV = vertexUV;
