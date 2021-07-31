31d8ea4949b0034ca38320960d5d6fed2c7627f9
Bricktheworld <brandon.shihabi@gmail.com>
2021-07-31 16:21:07 -0700

    Multiple point lights and directional lights

--- res/shaders/forward_material.glsl
+++ res/shaders/forward_material.glsl
@@ -59,25 +59,25 @@ struct PointLight {
   float linear;
   float quadratic;
 
-  vec4 ambient;
-  vec4 diffuse;
+  vec3 ambient;
+  vec3 diffuse;
   float specular;
 };
 
-#define MAX_POINT_LIGHTS 1000
-// layout (std140) uniform PointLights {
-//   vec4 positions[MAX_POINT_LIGHTS];
-//
-//   float constants[MAX_POINT_LIGHTS];
-//   float linears[MAX_POINT_LIGHTS];
-//   float quadratics[MAX_POINT_LIGHTS];
-//
-//   vec4 ambients[MAX_POINT_LIGHTS];
-//   vec4 diffuses[MAX_POINT_LIGHTS];
-//   float speculars[MAX_POINT_LIGHTS];
-//
-//   int count;
-// } ub_Point_lights;
+#define MAX_POINT_LIGHTS 512
+layout (std140) uniform PointLights {
+  vec4 positions[MAX_POINT_LIGHTS];
+
+  vec4 constants[MAX_POINT_LIGHTS];
+  vec4 linears[MAX_POINT_LIGHTS];
+  vec4 quadratics[MAX_POINT_LIGHTS];
+
+  vec4 ambients[MAX_POINT_LIGHTS];
+  vec4 diffuses[MAX_POINT_LIGHTS];
+  vec4 speculars[MAX_POINT_LIGHTS];
+
+  int count;
+} ub_Point_lights;
 
 uniform vec4 u_Tint;
 
@@ -85,45 +85,65 @@ uniform sampler2D u_Diffuse;
 uniform sampler2D u_Specular;
 uniform float u_Shininess;
 
-// uniform DirectionalLight u_Directional_light;
-uniform PointLight u_Point_lights[MAX_POINT_LIGHTS];
-uniform int u_Point_light_count;
-
 uniform mat3 u_Normal_matrix;
 
-vec3 calc_point_light(PointLight light, vec3 normal, vec3 view_direction);
+vec3 calc_point_light(PointLight light, vec3 normal, vec3 view_direction, vec3 material_diffuse, vec3 material_specular);
+vec3 calc_directional_light(vec3 normal, vec3 view_direction, vec3 material_diffuse, vec3 material_specular);
 
 void main()
 {    
     vec3 normal = normalize(u_Normal_matrix * f_Normal);
     vec3 view_direction = normalize(ub_Camera.position.xyz - f_Position);
 
-    vec3 result = vec3(0);
-    for(int i = 0; i < u_Point_light_count; i++) {
-	result += calc_point_light(u_Point_lights[i], normal, view_direction);
+    vec3 material_diffuse = vec3(texture(u_Diffuse, f_UV));
+    vec3 material_specular = vec3(texture(u_Specular, f_UV));
+
+    vec3 result = calc_directional_light(normal, view_direction, material_diffuse, material_specular);
+
+    for(int i = 0; i < 2; i++) {
+	PointLight point_light = PointLight(ub_Point_lights.positions[i].xyz, ub_Point_lights.constants[i].x, ub_Point_lights.linears[i].x, ub_Point_lights.quadratics[i].x, ub_Point_lights.ambients[i].xyz, ub_Point_lights.diffuses[i].xyz, ub_Point_lights.speculars[i].x);
+	result += calc_point_light(point_light, normal, view_direction, material_diffuse, material_specular);
     }
 
     result *= u_Tint.xyz;
     color = vec4(result, 1.0f);
 }
 
-vec3 calc_point_light(PointLight light, vec3 normal, vec3 view_direction) 
-{
-    vec3 ambient = light.ambient.xyz * light.diffuse.xyz * vec3(texture(u_Diffuse, f_UV));
+vec3 calc_directional_light(vec3 normal, vec3 view_direction, vec3 material_diffuse, vec3 material_specular) 
+{ 
+    if(ub_Directional_light.exists == 0) 
+	return vec3(0);
+    vec3 ambient = ub_Directional_light.ambient.xyz * ub_Directional_light.diffuse.xyz * material_diffuse;
 
-    vec3 light_direction = normalize(light.position.xyz - f_Position);
+    vec3 light_direction = normalize(-ub_Directional_light.direction.xyz);
 
     float diffuse_impact = max(dot(normal, light_direction), 0.0);
-    vec3 diffuse = diffuse_impact * light.diffuse.xyz * vec3(texture(u_Diffuse, f_UV));
+    vec3 diffuse = diffuse_impact * ub_Directional_light.diffuse.xyz * material_diffuse;
 
     vec3 reflect_direction = reflect(-light_direction, normal);
 
     float spec = pow(max(dot(view_direction, reflect_direction), 0.0), u_Shininess);
-    vec3 specular = light.specular * spec * light.diffuse.xyz * vec3(texture(u_Specular, f_UV));
+    vec3 specular = ub_Directional_light.specular * spec * ub_Directional_light.diffuse.xyz * material_specular;
+
+    return ambient + diffuse + specular;
+}
+
+vec3 calc_point_light(PointLight light, vec3 normal, vec3 view_direction, vec3 material_diffuse, vec3 material_specular) 
+{
+    vec3 ambient = light.ambient.xyz * light.diffuse.xyz * material_diffuse;
+
+    vec3 light_direction = normalize(light.position.xyz - f_Position);
+
+    float diffuse_impact = max(dot(normal, light_direction), 0.0);
+    vec3 diffuse = diffuse_impact * light.diffuse.xyz * material_diffuse;
+
+    vec3 reflect_direction = reflect(-light_direction, normal);
+
+    float spec = pow(max(dot(view_direction, reflect_direction), 0.0), u_Shininess);
+    vec3 specular = light.specular * spec * light.diffuse.xyz * material_specular;
 
     // Point light attenuation
     float distance = length(light.position.xyz - f_Position);
     float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
-
     return (ambient + diffuse + specular) * attenuation;
 }
